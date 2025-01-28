10d7cc789ccba09135c4039996427eebf3167fe6
phoethingyan-tech <phoethingyan.tech@gmail.com>
2025-01-28 14:35:57 +0630

    started

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    echo "<h1>Hello PHP</h1>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <script>
+        document.write('<h1>Hello JS</h1>');
+    </script>    
+</body>
+</html>
